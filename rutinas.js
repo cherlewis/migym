24a79953fc8a74fb30042a6185656df9d6846c45
cherlewis <cherlewis1@gmail.com>
2026-03-09 12:22:34 +0100

    Update rutinas.js

--- rutinas.js
+++ rutinas.js
@@ -1,5 +1,6 @@
 const ejerciciosDeHoy = [
     { nombre: "Press de Banca", musculo: "Pecho", series: 4, emoji: "💪" },
     { nombre: "Dominadas", musculo: "Espalda", series: 3, emoji: "🦍" },
-    { nombre: "Sentadillas", musculo: "Piernas", series: 4, emoji: "🦵" }
+    { nombre: "Sentadillas", musculo: "Piernas", series: 4, emoji: "🦵" },
+    { nombre: "Sentadillas2", musculo: "Piernas", series: 4, emoji: "🦵" }
 ];
