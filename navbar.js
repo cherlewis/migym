c9d5a954e30fbd28d556dd14d8a221ae8d93ffcc
cherlewis <cherlewis1@gmail.com>
2026-03-10 21:00:54 +0100

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,25 +1,23 @@
 // Guardamos todo el diseño de la barra en una variable
 const menuInferior = `
     <div class="pb-24"></div>
-
     <div class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-100 flex justify-around items-center pt-3 pb-6 shadow-[0_-5px_15px_rgba(0,0,0,0.03)] z-50">
         
-        <a href="index.html" class="flex flex-col items-center text-blue-600 hover:text-blue-700 transition duration-200">
+        <a href="index.html" class="flex flex-col items-center text-gray-400 hover:text-blue-600 transition duration-200">
             <div class="text-2xl mb-1">🏠</div>
-            <span class="text-[11px] font-bold">Inicio</span>
+            <span class="text-[11px] font-medium">Inicio</span>
         </a>
 
-        <a href="#" class="flex flex-col items-center text-gray-400 hover:text-blue-500 transition duration-200">
-            <div class="text-2xl mb-1">📋</div>
-            <span class="text-[11px] font-medium">Rutinas</span>
+        <a href="admin.html" class="flex flex-col items-center text-gray-400 hover:text-blue-600 transition duration-200">
+            <div class="text-2xl mb-1">⚙️</div>
+            <span class="text-[11px] font-medium">Admin</span>
         </a>
 
-        <a href="perfil.html" class="flex flex-col items-center text-gray-400 hover:text-blue-500 transition duration-200">
+        <a href="perfil.html" class="flex flex-col items-center text-gray-400 hover:text-blue-600 transition duration-200">
             <div class="text-2xl mb-1">👤</div>
             <span class="text-[11px] font-medium">Perfil</span>
         </a>
 
     </div>
 `;
-
 document.body.insertAdjacentHTML('beforeend', menuInferior);
